e573dd3c29e3e132d8cba38e1d97781537dfe47c
Vladimir Djurovic <vdjurovic@bitshifted.co>
2025-02-12 15:19:25 +0100

    fix: ansible custom SSH connection configuration (#84)

---
+++ exec/setup_test.go
@@ -0,0 +1,57 @@
+// Copyright 2025 Bitshift D.O.O
+// SPDX-License-Identifier: MPL-2.0
+
+package exec
+
+import (
+	"testing"
+
+	"github.com/bitshifted/liftoff/config"
+	"github.com/bitshifted/liftoff/log"
+	"github.com/stretchr/testify/suite"
+)
+
+type ExecutionSetupTestSuite struct {
+	suite.Suite
+}
+
+func (ts *ExecutionSetupTestSuite) SetupSuite() {
+	// Initialize logger
+	log.Init(true)
+	log.Logger.Info().Msg("Running ExecutionSetupTestSuite")
+}
+
+func TestExecutionSetuptestSuite(t *testing.T) {
+	suite.Run(t, new(ExecutionSetupTestSuite))
+}
+
+func (ts *ExecutionSetupTestSuite) TestSshConfigFile_NoBastion() {
+	ec := &ExecutionConfig{
+		Config: &config.Configuration{
+			ProcessingVars: map[string]interface{}{
+				"ansible_user":            "testuser",
+				"ansible_ssh_private_key": "/path/to/private/key",
+			},
+		},
+		ConfigFilePath: "/path/to/config.yaml",
+	}
+	err := ec.generateSSHConfig()
+	ts.NoError(err)
+	ts.Equal("/tmp/ssh_config_2f706174", ec.Config.ProcessingVars["ssh_config_file"])
+}
+
+func (ts *ExecutionSetupTestSuite) TestSshConfigFile_WithBastion() {
+	ec := &ExecutionConfig{
+		Config: &config.Configuration{
+			ProcessingVars: map[string]interface{}{
+				"ansible_user":            "testuser",
+				"ansible_ssh_private_key": "/path/to/private/key",
+				"ansible_bastion_host":    "bastion.example.com",
+			},
+		},
+		ConfigFilePath: "/bastion/config.yaml",
+	}
+	err := ec.generateSSHConfig()
+	ts.NoError(err)
+	ts.Equal("/tmp/ssh_config_2f626173", ec.Config.ProcessingVars["ssh_config_file"])
+}
